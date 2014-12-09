942d315f469a3d7c5b2acc1533d52816ff054b33
Jin Zhang <jinzhang.zj@gmail.com>
2014-12-09 13:56:49 -0600

    merge cdSVM.h to collaborative_ranking.h

--- collaborative_ranking.h
+++ collaborative_ranking.h
@@ -258,13 +258,19 @@ Output:
 using dual coordinate descent with the equation listed in the report
 */
 
-// learning U, return w vector
-vector<double> trainU(problem* p, parameter* param) {
+// helper function in generating random integer between [start, end];
+int myrandom (int i) {
+        mt19937 gen(time(NULL));
+        uniform_int_distribution<int> randidx(0, i + 1);
+	return randidx(gen);
+}
+
+// learning U, with global w and alpha vector
+// w is the U[iusr ... iusr+rank]
+void trainU(problem* p, parameter* param, double* U, int iusr, double* alpha) {
 	vector<int> order(p->l);
-	vector<double> alpha(p->l, 0);
-	vector<double> w(p->n, 0);
 	double one_2C = 0.5 / param->C;
-	double maxiter = 1000;
+	double maxiter = 10;
 	double eps = param->eps;
 	double oldQ = 0;
 
@@ -273,9 +279,10 @@ vector<double> trainU(problem* p, parameter* param) {
 	}
 
 	for (int iter = 0; iter < maxiter; ++iter) {
-		random_shuffle(order.begin(), order.end() );
+		// random_shuffle(order.begin(), order.end(), myrandom );
 		for (int i = 0; i < p->l; ++i) {
 			int idx = order[i];
+
 			feature_node* xi = p->x[idx];
 
 			double xi_snorm = 0;
@@ -285,16 +292,22 @@ vector<double> trainU(problem* p, parameter* param) {
 
 			double ywxi = 0;
 			for (int j = 0; j < p->n; ++j) {
-				ywxi += xi[j].value * w[xi[j].index - 1];
+				ywxi += xi[j].value * U[iusr * p->n + j];
 			}
-			ywxi *= p->y[idx];
+
+			//if (ywxi != ywxi) {
+			//	printf("break ywxi: %f\n", ywxi);
+			//	exit(0);
+			//}
 
 			double delta = (1 - ywxi - alpha[idx] * one_2C) / (xi_snorm + one_2C);
+
 			delta = max(0., delta + alpha[idx]) - alpha[idx];
 			alpha[idx] += delta;
 			
 			for (int j = 0; j < p->n; ++j) {
-				w[xi[j].index - 1] += delta * p->y[idx] * xi[j].value;
+				double dval = delta * xi[j].value;
+				U[idx * p->n + j] += dval;
 			}
 		}
 		
@@ -303,7 +316,7 @@ vector<double> trainU(problem* p, parameter* param) {
 		double alpha_sum = 0;
 		double Q = 0;
 		for (int j = 0; j < p->n; ++j) {
-			w_snorm += w[j] * w[j];
+			w_snorm += U[iusr * p->n + j] * U[iusr * p->n + j];
 		}
 		for (int j = 0; j < p->l; ++j) {
 			alpha_snorm += alpha[j] * alpha[j];	
@@ -315,52 +328,59 @@ vector<double> trainU(problem* p, parameter* param) {
 		}
 		oldQ = Q;
 	}
-	return w;
 }
 
-// learning V, return w vector
-// the input is only one sample, or p->n = 1
-vector<double> trainV(problem* p, parameter* param) {
-	double alpha = 0;
+// learning V with global w and alpha vector 
+// w is V[iid1 .. iid1+rank], -V[iid2 .. iid2+rank]
+void trainV(problem* p, parameter* param, double* V, comparison& c, double* alpha ) {
 	double one_2C = 0.5 / param->C;
-	double maxiter = 1000;
+	double maxiter = 10;
 	double eps = param->eps;
 	double oldQ = 0;  
-	vector<double> w(p->n);
 	feature_node* xi = p->x[0];
+
+	int uid = c.user_id;
+	int iid1 = c.item1_id;
+	int iid2 = c.item2_id;
+	int rank = p->n / 2;
 
 	for (int iter = 0; iter < maxiter; ++iter) {
 		double xi_snorm = 0;
-		for (int j = 0; j < p->n; ++j) {
+		for (int j = 0; j < rank; ++j) {
 			xi_snorm += xi[j].value * xi[j].value;
 		}
+		xi_snorm *= 2;
 
 		double ywxi = 0;
-		for (int j = 0; j < p->n; ++j) {
-			ywxi += xi[j].value * w[xi[j].index - 1];
+		for (int j = 0; j < rank; ++j) {
+			ywxi += xi[j].value * V[iid1 * rank + j];
+			ywxi += xi[j + rank].value * V[iid2 * rank + j];
 		}
 
-		double delta = (1 - ywxi - alpha * one_2C) / (xi_snorm * 2 + one_2C);		// xi_snorm * 2, this is the only difference
-		delta = max(0., delta + alpha) - alpha;
-		alpha += delta;
-
-		for (int j = 0; j < p->n; ++j) {
-			w[xi[j].index - 1] += delta * p->y[0] * xi[j].value;
-		}
-
-		// calculating the objective function value
-		double Q = 0;
-		for (int j = 0; j < p->n; ++j) {
-			Q += w[j] * w[j];
-		}
-		Q = Q / 2 + alpha * alpha * one_2C / 2 - alpha;
+		double delta = (1 - ywxi - alpha[0] * one_2C) / (xi_snorm * 2 + one_2C);		// xi_snorm * 2, this is the only difference
 		
+		delta = max(0., delta + alpha[0]) - alpha[0];
+		alpha[0] += delta;
+		for (int j = 0; j < rank; ++j) {
+			double dval = delta * xi[j].value;
+			//#pragma omp atomic
+			V[iid1 * rank + j] += dval;
+			//#pragma omp atomic
+			V[iid2 * rank + j] -= dval;
+		}
+
+		double Q = 0;
+		double w_snorm = 0;
+		for (int j = 0; j < rank; ++j) {
+			w_snorm += V[iid1 * rank + j] * V[iid1 * rank + j];
+			w_snorm += V[iid2 * rank + j] * V[iid2 * rank + j];
+		}
+		Q = w_snorm / 2 - alpha[0] * alpha[0] * one_2C / 2 - alpha[0];
 		if (fabs(Q - oldQ) < eps) {
 			break;
 		}
 		oldQ = Q;
 	}
-	return w;
 }
 
 #endif
